22a4a63fd0277bf206ce4108c68663f3b560ccf5
Damian Gryski <dgryski@gmail.com>
2016-11-19 05:31:19 +0100

    unroll varint decode1 loop

--- varint.go
+++ varint.go
@@ -10,21 +10,22 @@ func Encode1(dst []byte, n uint32) []byte {
 }
 
 func Decode1(dst *uint32, src []byte) int {
-
-	var n uint32
-
-	var shift uint
-	for i, s := range src[:4] {
-		if s < 0x80 {
-			n |= uint32(s) << shift
-			*dst = n
-			return i + 1
-		}
-
-		n |= uint32(s&^0x80) << shift
-		shift += 7
+	*dst = uint32(src[0]) &^ 0x80
+	if src[0] < 128 {
+		return 1
 	}
-
-	*dst = n | uint32(src[4])<<shift
+	*dst = (uint32(src[1]&^0x80) << 7) | *dst
+	if src[1] < 128 {
+		return 2
+	}
+	*dst = (uint32(src[2]&^0x80) << 14) | *dst
+	if src[2] < 128 {
+		return 3
+	}
+	*dst = (uint32(src[3]&^0x80) << 21) | *dst
+	if src[3] < 128 {
+		return 4
+	}
+	*dst = (uint32(src[4]&^0x80) << 28) | *dst
 	return 5
 }
